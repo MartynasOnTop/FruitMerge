4ba11bb8cee74d1592feb3632905f0ebdcf83105
Martynas <ponasniekas10@gmail.com>
2024-03-14 14:50:31 +0200

    sounds menu

--- Assets/Scripts/Combining.cs
+++ Assets/Scripts/Combining.cs
@@ -6,6 +6,9 @@ public class Combining : MonoBehaviour
 {
     public GameObject betterFruit;
     public int fruitPoints;
+    public Color fruitColor;
+    public ParticleSystem combiningPart;
+    public AudioClip mergeSound;
     private async void OnCollisionEnter2D(Collision2D collision)
     {
         if (gameObject.tag == collision.gameObject.tag)
@@ -20,18 +23,43 @@ public class Combining : MonoBehaviour
             }
             await new WaitForSeconds(0.1f);
 
+            if (gameObject.GetComponent<Combining>() == null) return;
+            
             if (betterFruit != null)
             {
+                Sound();
+                Particles();
                 var newFruit = Instantiate(betterFruit, transform.position, transform.rotation);
                 newFruit.GetComponent<Falling>().isInAir = false;
+                newFruit.GetComponent<Falling>().isLanded = true;
                 Spawner.score += fruitPoints;
+
+                await new WaitForSeconds(0.1f);
                 Destroy(gameObject);
             }
+
+            if (gameObject.GetComponent<Combining>() == null) return;
+
             if (betterFruit == null)
             {
+                Sound();
+                Particles();
                 Spawner.score += fruitPoints;
+
+                await new WaitForSeconds(0.1f);
                 Destroy(gameObject);
             }
         }
+    }
+
+    public void Particles()
+    {
+        var prt = Instantiate(combiningPart, transform.position, transform.rotation);
+        prt.startColor = fruitColor;
+    }
+    public void Sound()
+    {
+        var source = gameObject.GetComponent<AudioSource>();
+        source.PlayOneShot(mergeSound);
     }
 }
--- Assets/Scripts/Falling.cs
+++ Assets/Scripts/Falling.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,9 @@ public class Falling : MonoBehaviour
 {
     public Spawner spawner;
     public bool isInAir = true;
+    public bool isLanded = false;
+    public AudioClip fallSound;
+    public AudioSource gameOver;
     private async void Update()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -14,15 +18,28 @@ public class Falling : MonoBehaviour
         {
             if (isInAir == false) return;
 
+            GetComponent<AudioSource>().PlayOneShot(fallSound);
+
             isInAir = false;
             transform.position = new Vector2(mousePos.x, transform.position.y);
             var rg = gameObject.GetComponent<Rigidbody2D>();
             rg.gravityScale = 10;
             await new WaitForSeconds(1f);
+            isLanded = true;
             spawner.Spawn();
         }
-        if (transform.position.y > 0)
+    }
+    private async void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "DeadLine" && isLanded == true)
         {
+            foreach (var item in FindSceneObjectsOfType(typeof(Combining))) 
+            {
+                item.GetComponent<Combining>().Particles();
+                Destroy(item.GameObject());
+            }
+            Instantiate(gameOver);
+            await new WaitForSeconds(1f);
             SceneManager.LoadScene("Menu");
         }
     }
